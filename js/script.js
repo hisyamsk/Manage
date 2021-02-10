7db132da031bafd6330ef67ff193954470169a87
hisyamsk <hisyamskurniawan18@gmail.com>
2021-02-10 22:24:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//         var swiper = new Swiper('.swiper-container', {
+//             autoplay: {
+//                 delay: 5000,
+//                 disableOnInteraction: false,
+//             },
+//             loop: true,
+//             slidesPerView: 1,
+//             spaceBetween: 0,
+//             centeredSlides: true,
+//             pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//             },
+//         });
+//     } else {
+//         var swiper = new Swiper('.swiper-container', {
+//             autoplay: {
+//                 delay: 5000,
+//                 disableOnInteraction: false,
+//             },
+//             loop: true,
+//             slidesPerView: 3,
+//             spaceBetween: 30,
+//             centeredSlides: true,
+//         });
+//     }
+// }
+
+// var x = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
+
+var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+});
